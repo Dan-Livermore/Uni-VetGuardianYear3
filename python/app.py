8de3f253f5b1682693762fd227ccc197afbe816f
Dan Livermore <danieljlivermore@gmail.com>
2023-11-27 16:07:50 +0000

    Several issues fixed but still unable to output predictions

--- python/app.py
+++ python/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, jsonify
+import jsonpickle
+import json
 import tensorflow as tf
 from PIL import Image
-from tensorflow.keras.preprocessing import image as keras_image
+#from tensorflow.keras.preprocessing import image as keras_image
 import numpy as np
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -12,42 +14,38 @@ def hello_world():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def predict():
-    class_names = [
-        'Abyssian','American Shorthair','African Grey Parrot','Boxer','Bulldog','British Shorthair','Beagle','Budgerigar',"Bourke's Parakeet",
-        'Cockatoo','Canary','Cocker Spaniel','Dachsund','Cockatiel','Chinchillas','Burmese','Egyptian Mau','Gerbils','Devon Rex','Finch',
-        'German Shepard','French Bulldog','Horses','Hedgehogs','Himalayan','Golden Retriever','Guinea Pigs','Great Dane','Hamsters','Mice',
-        'Parrotlet','Miniature Schanauzer','Lovebird','Labrador Retriever','Pionus Parrot','Maine Coon','Persian','Ragdoll','Rottweiler',
-        'Quaker Parrot','Rats','Rabbits','Poodle','Russain Blue','Serval Cats','Shih Tzu','Sphynx','Siamese','Scottish Fold','Yorkshire Terrier']
-        
+def load_and_prep_image():
     imagefile= request.files['imagefile']
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)
-
-    # image = Image.open(image_path) 
-    # img = Image.open(image_path)
-    # img = tf.image.decode_image(img, channels=3)
-    # img = tf.image.resize(img, size = [150,150])
-    # img = img/255.0
-
     img = tf.io.read_file(image_path)
-    img = tf.image.decode_image(img, channels=3)  # Decoding the image with 3 color channels
-    img = tf.image.resize(img, size=[150, 150])
-    img = tf.cast(img, tf.float32) / 255.0
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.resize(img, size = [150,150])
+    img = img/255.
+    return img
 
-    Savedmodel = tf.keras.models.load_model('../python/Model.h5')
-
-    #predictions = Savedmodel.predict(img)
-    predictions = Savedmodel.predict(tf.expand_dims(img, axis=0))
-
-    if len(predictions[0]) > 1:
-        pred_class = class_names[predictions.argmax()]
+def pred_and_plot(model, class_names):
+    img = load_and_prep_image()
+    pred = model.predict(tf.expand_dims(img, axis=0))
+    if len(pred[0]) > 1:
+        pred_class = class_names[pred.argmax()]
     else:
-        pred_class = class_names[int(tf.round(predictions)[0][0])]
+        pred_class = class_names[int(tf.round(pred)[0][0])]
+    return str(pred_class)
 
-    #classification = '%s (%.2f%%)' % (label[1], label[2]*100)
-    return render_template('index.html',predictions=pred_class)
+class_names = [
+    'Abyssinian','African Grey Parrot','American Shorthair','Beagle',"Bourke's Parakeet" 'Boxer','British Shorthair','Budgerigar','Bulldog',
+    'Burmese','Canary','Chinchillas','Cockatiel','Cockatoo','Cocker Spaniel','Dachshund','Devon Rex','Egyptian Mau','Finch','French Bulldog',
+    'Gerbils','German Shepherd','Golden Retriever','Great Dane','Guinea Pigs','Hamsters','Hedgehogs','Himalayan','Horses','Labrador Retriever',
+    'Lovebird','Maine Coon','Mice','Miniature Schnauzer','Parrotlet','Persian','Pionus Parrot','Poodle','Quaker Parrot','Rabbits','Ragdoll',
+    'Rats','Rottweiler','Russian Blue','Scottish Fold','Serval Cats','Shih Tzu','Siamese','Sphynx','Yorkshire Terrier']
 
+@app.route('/', methods=['POST'])
+def modelling():
+    model = tf.keras.models.load_model('Model.h5')
+    print('Model Loaded!')
+    pred = pred_and_plot(model, class_names)
+    return render_template('index.html', predictions=json.dumps(pred))
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
