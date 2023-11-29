ea0f72efb5f1433c8037a002b7cfbe8ca6dbba5f
Dan Livermore <danieljlivermore@gmail.com>
2023-11-29 15:27:55 +0000

    Commented navbar an have updated colours of all elements

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,55 +1,73 @@
 import { Link } from "react-scroll";
 import { useState } from "react";
-import {FaTimes} from "react-icons/fa";
-import {CiMenuFries} from "react-icons/ci";
+import { FaTimes } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 
+// Navbar
 const Nav = () => {
+  // To allow dynamic navbar
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
-  const content = <>
-  <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-slate-900 transition">
-    <ul className="text-center text-xl p-20">
-      <Link spy={true} smooth={true} to="Home">
-        <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded spy={true} smooth={true}">Home</li>
-      </Link>
-      <Link spy={true} smooth={true} to="SymptomChecker">
-        <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded spy={true} smooth={true}">Symptom Checker</li>
-      </Link>
-      <Link spy={true} smooth={true} to="Accounts">
-        <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded spy={true} smooth={true}">Accounts</li>
-      </Link>
-    </ul>
-  </div></>
+
+  // Mobile view
+  const content = (
+    <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-teal-500 transition">
+      <ul className="text-center text-xl p-20">
+        {/* Links for sections*/}
+        <Link spy={true} smooth={true} to="Home">
+          <li className="my-4 py-4 border-b border-teal-500 hover:bg-teal-500 hover:text-zinc-400 hover:rounded cursor-pointer spy={true} smooth={true}">Home</li>
+        </Link>
+        <Link spy={true} smooth={true} to="SymptomChecker">
+          <li className="my-4 py-4 border-b border-teal-500 hover:bg-teal-500 hover:text-zinc-400 hover:rounded cursor-pointer spy={true} smooth={true}">Symptom Checker</li>
+        </Link>
+        <Link spy={true} smooth={true} to="Accounts">
+          <li className="my-4 py-4 border-b border-teal-500 hover:bg-teal-500 hover:text-zinc-400 hover:rounded cursor-pointer spy={true} smooth={true}">Accounts</li>
+        </Link>
+      </ul>
+    </div>
+  );
+
+  // Return JSX for the navbar
   return (
-    <nav>
+    <nav className="bg-teal-600">
       <div className="h-10vh flex justify-between z-50 text-white lg:py-5 px-20 py-4">
+        {/* Logo */}
         <div className="flex items-center flex-1">
-          <span className="text-3xl font-bold">Vet Guardian</span>
+          <span className="text-3xl font-bold  inline-flex items-center">
+            <img src="../favicon.ico" alt="Vet Guardian" className="logo-img inline-block h-8 w-auto mr-2" />
+            Vet Guardian
+          </span>
         </div>
+        
+        {/* Desktop view */}
         <div className="lg:flex md:flex lg: flex-1 items center justify-end font-normal hidden">
           <div className="flex-10">
-          <ul className="flex gap-8 mr-16 text-[18px]">
-      <Link spy={true} smooth={true} to="Home">
-        <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Home</li>
-      </Link>
-      <Link spy={true} smooth={true} to="SymptomChecker">
-        <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Symptom Checker</li>
-      </Link>
-      <Link spy={true} smooth={true} to="Accounts">
-        <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">Accounts</li>
-      </Link>
-    </ul>
+            <ul className="flex gap-8 mr-16 text-[18px]">
+              {/* links for sections */}
+              <Link spy={true} smooth={true} to="Home">
+                <li className="hover:text-zinc-400 transition border-b-0 border-teal-600 hover:border-zinc-400 cursor-pointer">Home</li>
+              </Link>
+              <Link spy={true} smooth={true} to="SymptomChecker">
+                <li className="hover:text-zinc-400 transition border-b-0 border-teal-600 hover:border-zinc-400 cursor-pointer">Symptom Checker</li>
+              </Link>
+              <Link spy={true} smooth={true} to="Accounts">
+                <li className="hover:text-zinc-400 transition border-b-0 border-teal-600 hover:border-zinc-400 cursor-pointer">Accounts</li>
+              </Link>
+            </ul>
           </div>
         </div>
+
+        {/* Mobile burger */}
         <div>
           {click && content}
         </div>
         <button className="block sm:hidden transition" onClick={handleClick}>
-          { click ? <FaTimes/> : <CiMenuFries/> }
+          {/* Hamburger icons*/}
+          { click ? <FaTimes /> : <GiHamburgerMenu /> }
         </button>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
