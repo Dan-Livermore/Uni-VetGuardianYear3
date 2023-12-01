304979ae069b3e4c779af2181fc4ec78c1ce8e6b
Dan Livermore <danieljlivermore@gmail.com>
2023-12-01 11:27:39 +0000

    Hosted python model and accessible from react front end

--- client/src/Pages/SymptomChecker.jsx
+++ client/src/Pages/SymptomChecker.jsx
@@ -1,7 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 const SymptomChecker = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [prediction, setPrediction] = useState('');
+
+  const handleImageChange = (event) => {
+    setSelectedImage(event.target.files[0]);
+  };
+
+  const handleImageUpload = async (event) => {
+    event.preventDefault(); // Prevent default form submission
+
+    const formData = new FormData();
+    formData.append('imagefile', selectedImage);
+
+    try {
+      const response = await axios.post('http://localhost:3002/predict', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      setPrediction(response.data.prediction);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div>SymptomChecker lets get that py</div>
-  )
+    <div>
+      <h1>Image Uploader</h1>
+      <form onSubmit={handleImageUpload}>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <button type="submit">Upload Image</button>
+      </form>
+
+      <h1>Prediction Result</h1>
+      {prediction && <p>Prediction: {prediction}</p>}
+    </div>
+  );
 }
 
-export default SymptomChecker
+export default SymptomChecker;
--- python/app.py
+++ python/app.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 import tensorflow as tf
 from PIL import Image
 from tensorflow.keras.preprocessing import image as keras_image
 import numpy as np
 
 app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "http://localhost:3001"}})
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return render_template('index.html')
 
+
+model = tf.keras.models.load_model('Model.h5')
 class_names = [
     'Abyssian','American Shorthair','African Grey Parrot','Boxer','Bulldog','British Shorthair','Beagle','Budgerigar',"Bourke's Parakeet",
     'Cockatoo','Canary','Cocker Spaniel','Dachsund','Cockatiel','Chinchillas','Burmese','Egyptian Mau','Gerbils','Devon Rex','Finch',
@@ -18,30 +22,28 @@ class_names = [
     'Quaker Parrot','Rats','Rabbits','Poodle','Russain Blue','Serval Cats','Shih Tzu','Sphynx','Siamese','Scottish Fold','Yorkshire Terrier']
 
 def prepareImage():
-    imagefile= request.files['imagefile']
+    imagefile = request.files['imagefile']
     image_path = "./images/" + imagefile.filename
     imagefile.save(image_path)
-    #Prepare image to same scale as model
     img = tf.io.read_file(image_path)
     img = tf.image.decode_image(img, channels=3)
     img = tf.image.resize(img, size=[150, 150])
     img = tf.cast(img, tf.float32) / 255.0
     return img
 
-def predictImage(model, class_names):
-    img = prepareImage()
+def predictImage(img, model, class_names):
     predictions = model.predict(tf.expand_dims(img, axis=0))
-    if len(predictions[0]) > 1:
-        pred_class = class_names[predictions.argmax()]
-    else:
-        pred_class = class_names[int(tf.round(predictions)[0][0])]
+    pred_class = class_names[predictions.argmax()]
     return pred_class
 
-@app.route('/', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    model = tf.keras.models.load_model('Model.h5')
-    output = predictImage(model, class_names)
-    return render_template('index.html',prediction=output)
+    img = prepareImage()
+    output = predictImage(img, model, class_names)
+    return jsonify({'prediction': output})
+
+if __name__ == '__main__':
+    app.run(port=3002, debug=True)
 
 if __name__ == '__main__':
     app.run(port=3002, debug=True)
