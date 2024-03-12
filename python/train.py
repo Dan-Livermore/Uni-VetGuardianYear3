661669bfd3144ac5201ed53811f45a46f28d4e7c
Dan Livermore <danieljlivermore@gmail.com>
2024-03-12 14:36:53 +0000

    Image Model Ran for 512 size 30 epochs

--- python/train.py
+++ python/train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8n-cls.pt')  # load the smallest size model
 
 model.train(data='./dataset',
-            epochs=20, imgsz=384)
+            epochs=30, imgsz=512)
