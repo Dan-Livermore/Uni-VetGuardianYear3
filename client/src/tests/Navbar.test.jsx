a1864dd3a2c1eabb391d95d0d2a353dfa53da682
Dan Livermore <danieljlivermore@gmail.com>
2024-04-08 10:49:14 +0100

    Updating paths in testing

--- client/src/tests/Navbar.test.jsx
+++ client/src/tests/Navbar.test.jsx
@@ -1,10 +1,10 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import Navbar from '../components/Navbar';
+import Navbar from '\client\src\Components\Navbar.jsx';
 
 describe('Navbar', () => {
   it('CICD Test', () => {
-    // render(<Navbar />);
+    render(<Navbar />);
     expect(true).toBe(true);
   });
 });
